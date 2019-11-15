1563d98761f128d4e8d3684388416aa02830c581
DustinG98 <thedustingraham@gmail.com>
2019-11-15 10:23:51 -0700

    Completed MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  let { character } = props;
+  return (
+    <div key={character.id}>
+      <h1>{character.name}</h1>
+      <h4>Gender: {character.gender}</h4>
+      <p>Status: {character.status}</p>
+      <p>Species: {character.species}</p>
+    </div>
+  );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from "./CharacterCard";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  const { characters } = props
+  const [searchTerm, setSearchTerm] = useState("");
+  const [newCharacters, setNewCharacters] = useState([])
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+  }
+  const result = [];
+  useEffect(() => {
+      characters.forEach(cv => {
+        if(cv.name !== undefined) {
+          return cv.name.toLowerCase().includes(searchTerm.toLowerCase()) ? result.push(cv) : null
+        }
+      })
+    
+    if(result === []) {
+      setNewCharacters(characters)
+    }
+    else{
+      setNewCharacters(result)
+    }
+  }, [searchTerm])
+  
   return (
     <section className="search-form">
-     // Add a search form here
+     <input
+        type="text"
+        placeholder="Search By Name"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      {(searchTerm === "") ? characters.map(character => {
+        return <CharacterCard key={character.id} character={character}/>
+      }) : newCharacters.map(character => {
+        return <CharacterCard key={character.id} character={character}/>
+      })}
     </section>
   );
 }
