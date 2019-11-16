02ecea52b09e0b849863288bf932471e9178c6fb
DustinG98 <thedustingraham@gmail.com>
2019-11-16 11:29:04 -0700

    Live search complete with pagination

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,37 +1,19 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import React, { useState } from "react";
 import { Button } from 'reactstrap';
 import SearchForm from './SearchForm'
 
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [characters, setCharacters] = useState([]);
 
   const [page, setPage] = useState(1);
 
-  if(page === 0){
-    setPage(20);
-  } else if(page > 20){
-    setPage(1);
-  }
-
-
-  useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`)
-        .then(response => 
-          setCharacters(response.data.results))
-        .catch(err => console.log("Error", err))
-        
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [page]);
 
 
   return (
     <section className="character-list">
       <h1>Characters</h1>
-      <SearchForm characters={characters}/>
+      <SearchForm page={page} setPage={setPage}/>
       <div className="buttons">
           <Button className="pageButton" color="primary" onClick={() => setPage(page-1)} >Previous Page</Button>
           <h1>{page}</h1>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,25 +4,31 @@ import axios from 'axios'
 
 
 export default function SearchForm(props) {
-  const { characters } = props
+  const { characters, page, setPage } = props
   const [searchTerm, setSearchTerm] = useState("");
-  const [newCharacters, setNewCharacters] = useState([])
   const [searchResult, setSearchResults] = useState([]);
+
+
+
+  
+
 
   const handleChange = event => {
     setSearchTerm(event.target.value)
   }
   useEffect(() => {
-      const result = characters.filter(character => {
-        return character.name.toLowerCase().includes(searchTerm.toLowerCase());
-      })
-      setNewCharacters(result)
-      axios.get(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`)
+      axios.get(`https://rickandmortyapi.com/api/character/?name=${searchTerm}&page=${page}`)
       .then(response => {
         setSearchResults(response.data.results)
+        console.log(response)
+        if(page === 0){
+          setPage(response.data.info.pages);
+        } else if(page > response.data.info.pages){
+          setPage(1);
+        }
       })
     
-  }, [searchTerm, characters])
+  }, [searchTerm, characters, page, setPage])
   
   return (
     <section className="search-form">
@@ -33,9 +39,7 @@ export default function SearchForm(props) {
         onChange={handleChange}
       />
       <div className="characters">
-        {(searchTerm === "") ? newCharacters.map(character => {
-          return <div> <CharacterCard key={character.id} character={character}/> </div>
-        }) : searchResult.map(character => {
+        {searchResult.map(character => {
           return <CharacterCard key={character.id} character={character}/>
         }) }
       </div>
